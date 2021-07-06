d81e648c9565126ebae7c7cfccacc78b8f1cb239
Luke Wilson <luke@sitrobotsit.com>
2021-07-06 20:51:25 +0100

    spliting of form and fade fx

---
+++ foiprototype/main.js
@@ -0,0 +1,9 @@
+import {showElement, hideElement, saveSection, initSection, toggleElement } from './modules/ui.js';
+
+// alert("asdf");
+
+window.showElement = showElement;
+window.hideElement = hideElement;
+window.saveSection = saveSection;
+window.initSection = initSection;
+window.toggleElement = toggleElement;
