8d780eaca3da05bd4a0355d523c6c984f41ce80d
Luke Wilson <luke@sitrobotsit.com>
2021-07-26 17:00:08 +0200

    version 7 updated

---
+++ foi_prototype/v6b/main.js
@@ -0,0 +1,7 @@
+import {showElement, hideElement, saveSection, initSection, toggleElement } from './modules/ui.js';
+
+window.showElement = showElement;
+window.hideElement = hideElement;
+window.saveSection = saveSection;
+window.initSection = initSection;
+window.toggleElement = toggleElement;
